3c9f784ed74e0a97115e59d96f62db05769c84e2
arthur.grevin@gmail.com <arthur.grevin@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2014-12-19 22:28:48 +0000

    Changement de version de hibernate dans le pom.xml Ajout du hibernate.cfg.xml non complété en attente de la base de donnée Ajout du HibernateUtil.java

---
+++ trunk/SopRide/src/main/java/com/soprideshare/dao/HibernateUtil.java
@@ -0,0 +1,40 @@
+package com.soprideshare.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    
+    private static ServiceRegistry serviceRegistry;
+    
+    //Exécuter qu'une seule fois
+    private static SessionFactory buildSessionFactory() {
+        try {        	
+            
+        	// Create the SessionFactory from hibernate.cfg.xml            
+            Configuration configuration = new Configuration();
+			configuration.configure();
+			configuration.setProperty( "hibernate.jdbc.factory_class", "org.hibernate.jdbc.NonBatchingBatcherFactory" );
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(
+					
+			configuration.getProperties()).buildServiceRegistry();
+			// on retourne la sessionFactory
+			return configuration.buildSessionFactory(serviceRegistry);         
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    
+    // on peut accéder à la session UNIQUE (Singleton Pattern) 
+    // pour faire des requetes depuis n'importe où dans l'application 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
