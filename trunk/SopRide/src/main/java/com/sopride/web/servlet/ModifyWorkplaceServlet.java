4589110d804c95d206ef5b0d12a9915fb7a2ca60
jauffretpm@gmail.com <jauffretpm@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2014-12-28 14:11:15 +0000

    update fonctionnel, par contre pas optimisé : pour modifier : delete workplace then register new one

---
+++ trunk/SopRide/src/main/java/com/sopride/web/servlet/ModifyWorkplaceServlet.java
@@ -0,0 +1,65 @@
+package com.sopride.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sopride.core.beans.WorkplaceBE;
+import com.sopride.dao.WorkPlaceDAO;
+import com.sopride.web.util.WebUtils;
+
+/**
+ * Servlet implementation class ModifyWorkplaceServlet
+ */
+@WebServlet("/modifyworkplace")
+public class ModifyWorkplaceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModifyWorkplaceServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		WorkPlaceDAO DAO = WorkPlaceDAO.getInstance();
+		String id = request.getParameter("id");
+		WorkplaceBE workplace = DAO.getWorkplace(Integer.parseInt(id)); 
+		request.setAttribute("workplace", workplace);
+		WebUtils.forward(request, response, "modifyworkplace.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int Postcode = Integer.parseInt(request.getParameter("postcode"));
+		String City = request.getParameter("city");
+		String Street = request.getParameter("street");
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		WorkPlaceDAO DAO = WorkPlaceDAO.getInstance();
+		
+		WorkplaceBE workplace = new WorkplaceBE();	
+		workplace.setCity(City);
+		workplace.setId(id);
+		workplace.setPostCode(Postcode);
+		workplace.setStreet(Street);
+		
+		DAO.removeWorkPlace(id);
+		DAO.registerWorkplace(workplace);
+		
+		WebUtils.forward(request, response, "workplacemodified.jsp");
+		
+	}
+
+}
