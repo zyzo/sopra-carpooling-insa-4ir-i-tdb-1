52f33f32c4ec2d984c337133569b6b74b10496fb
arthur.grevin@gmail.com <arthur.grevin@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2015-01-04 23:51:14 +0000

    GetReport

---
+++ trunk/SopRide/src/main/java/com/sopride/web/servlet/GetReportServlet.java
@@ -0,0 +1,53 @@
+package com.sopride.web.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sopride.core.beans.UserBE;
+import com.sopride.dao.UserDAO;
+import com.sopride.web.util.WebUtils;
+
+/**
+ * Servlet implementation class GetReportServlet
+ */
+@WebServlet("/report")
+public class GetReportServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetReportServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserDAO DAO = UserDAO.getInstance();
+		List<UserBE> list = DAO.getAllUser();
+		int total = 0;
+		for(UserBE user : list){
+			total += user.getNumberOfConnections();
+		}
+		request.setAttribute("total", total);
+		request.setAttribute("list", list);
+		WebUtils.forward(request, response, "report.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
