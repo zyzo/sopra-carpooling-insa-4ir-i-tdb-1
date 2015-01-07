5ded50116fbd60e8bcbe8163b407cb6c2ec088a6
jauffretpm@gmail.com <jauffretpm@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2015-01-07 13:30:30 +0000

    Methode pour envoyer des mails dans webutil

--- SopRide/src/main/java/com/sopride/web/util/WebUtils.java
+++ SopRide/src/main/java/com/sopride/web/util/WebUtils.java
@@ -3,24 +3,82 @@ package com.sopride.web.util;
 import com.sopride.web.controller.UserCtrl;
 
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 
 public class WebUtils {
 
 	public static void forward(ServletRequest req, ServletResponse resp, String jspPage)
 			throws ServletException, IOException {
 		req.getServletContext()
-			.getRequestDispatcher("/WEB-INF/jsp/" + jspPage)
-			.forward(req,resp);
+		.getRequestDispatcher("/WEB-INF/jsp/" + jspPage)
+		.forward(req,resp);
 	}
 
 	public static UserCtrl getUserCtrl(ServletRequest req) {
 		HttpServletRequest request = (HttpServletRequest) req;
 		return (UserCtrl) request.getSession().getAttribute(WebConstants.SESSION_USER_CTRL);
 	}
+
+	public static void sendMail(String to,String subject,String content){
+
+		// Sender's email ID needs to be mentioned
+		String from = "soprideshare@gmail.com";
+		final String username = "soprideshare@gmail.com";//change accordingly
+		final String password = "carpooling";//change accordingly
+
+		// Assuming you are sending email through relay.jangosmtp.net
+		String host = "smtp.gmail.com";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "25");
+
+		// Get the Session object.
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+			// Create a default MimeMessage object.
+			Message message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// Set To: header field of the header.
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to));
+
+			// Set Subject: header field
+			message.setSubject(subject);
+
+			// Now set the actual message
+			message.setText(content);
+
+			// Send message
+			Transport.send(message);
+
+			System.out.println("Sent message successfully....");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
+
---
+++ SopRide/src/test/java/com/sopride/web/util/WebUtilsTest.java
@@ -0,0 +1,11 @@
+package com.sopride.web.util;
+
+public class WebUtilsTest {
+	public static void main (String[] args){
+		WebUtils.sendMail("jauffretp@yahoo.fr", "testsujet","testcontenu" );
+		
+	}
+
+
+}
+
