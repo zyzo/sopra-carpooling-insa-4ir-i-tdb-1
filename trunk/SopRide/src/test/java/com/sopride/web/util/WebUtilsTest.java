ea0aa51f7227471684cf3e3b7c536c4f3e5d901b
jauffretpm@gmail.com <jauffretpm@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2015-01-07 14:46:17 +0000

    Implémentation basique de forget password, il reste à gérer quand l'email n'existe pas dans la base de donnée.

--- trunk/SopRide/src/test/java/com/sopride/web/util/WebUtilsTest.java
+++ trunk/SopRide/src/test/java/com/sopride/web/util/WebUtilsTest.java
@@ -2,8 +2,7 @@ package com.sopride.web.util;
 
 public class WebUtilsTest {
 	public static void main (String[] args){
-		WebUtils.sendMail("jauffretp@yahoo.fr", "testsujet","testcontenu" );
-		
+		WebUtils.sendMail("soprideshare@gmail.com", "testsujet","testcontenu" );
 	}
 
 
