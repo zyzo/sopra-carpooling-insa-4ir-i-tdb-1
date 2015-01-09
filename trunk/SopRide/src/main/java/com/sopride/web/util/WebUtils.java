a6a3e2a35313ce9dd02992b24a5f7c0dd7e256c6
jauffretpm@gmail.com <jauffretpm@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2015-01-09 16:18:10 +0000

    methode mail html

--- trunk/SopRide/src/main/java/com/sopride/web/util/WebUtils.java
+++ trunk/SopRide/src/main/java/com/sopride/web/util/WebUtils.java
@@ -78,5 +78,56 @@ public class WebUtils {
 		}
 	}
 
+	
+	public static void sendMailHTML(String to,String subject,String content){
+
+		// Sender's email ID 
+		String from = "soprideshare@gmail.com";
+		final String username = "soprideshare@gmail.com";
+		final String password = "carpooling";
+
+		String host = "smtp.gmail.com";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "25");
+
+		// Get the Session object.
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+			// Create a default MimeMessage object.
+			Message message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// Set To: header field of the header.
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to));
+
+			// Set Subject: header field
+			message.setSubject(subject);
+
+			// Now set the actual message
+			message.setContent(content, "text/html");
+
+			// Send message
+			Transport.send(message);
+
+			//System.out.println("Sent message successfully....");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 }
 
