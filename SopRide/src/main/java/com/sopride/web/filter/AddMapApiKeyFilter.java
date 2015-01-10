e6056cfa06c35b9e68f244e22fad4ef664184274
danghaian168@gmail.com <danghaian168@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2015-01-10 00:18:23 +0000

    motherfuckers, i present to you EMBED GOOGLE MAP

---
+++ SopRide/src/main/java/com/sopride/web/filter/AddMapApiKeyFilter.java
@@ -0,0 +1,29 @@
+package com.sopride.web.filter;
+
+import com.sopride.web.util.WebConstants;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Add google map api key to the pages that actually need it.
+ */
+@WebFilter(filterName = "AddMapApiKeyFilter", urlPatterns =
+        {
+                WebConstants.PATH_MANAGE_WORKPLACE,
+        })
+public class AddMapApiKeyFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setAttribute(WebConstants.ATT_GOOGLE_API_KEY, "AIzaSyAWtwnk0kNJlxITog0xf9y7EDYaywrnUDk");
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
