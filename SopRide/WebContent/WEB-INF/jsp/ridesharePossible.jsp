23c77629557060eddcf91ad1e6689a01d31b41da
philippe.rouvier.mail@gmail.com <philippe.rouvier.mail@gmail.com@7736f2d4-b4a2-d4bb-7987-533447cc66d0>
2015-01-11 00:43:17 +0000

    SearchForRideshare : Gestion de plusieurs trajets OK reste à faire : améliorer la présentation

--- SopRide/WebContent/WEB-INF/jsp/ridesharePossible.jsp
+++ SopRide/WebContent/WEB-INF/jsp/ridesharePossible.jsp
@@ -2,108 +2,112 @@
 
 	<div class="jumbotron">
 		<div class="container">
-			<h3>Trajets possibles le lundi avec :</h3>
+			<h3>Trajets possibles le lundi :</h3>
 			<ul>
-				<c:if test="${empty list_lundi}">
+				<c:if test="${empty list_lundi[0]}">
 					<p>Pas de trajets disponibles ce jour la !</p>
 				</c:if>
-				<c:if test="${!empty list_lundi }">
-					<c:forEach var="rideshare" items="${list_lundi}">
-						<li>${rideshare.first_name} ${rideshare.last_name}
-							${rideshare.email} (${rideshare.email} / ${rideshare.phone})</li>
+				<c:if test="${!empty list_lundi[0] }">
+					 <c:forEach var="i" begin="0" end="${nb_trajets}" step="1">
+					 <p>Pour le trajet <c:out value="${trajets[i]}"/> :</p>
+						<c:forEach var="ride" items="${list_lundi[i]}">
+								<li>${ride.first_name} ${ride.last_name}
+									${ride.email} (${ride.email} / ${ride.phone})</li>
+						</c:forEach>
 					</c:forEach>
 				</c:if>
 			</ul>
-			<h3>Trajets possibles le mardi avec :</h3>
+			<h3>Trajets possibles le mardi :</h3>
 			<ul>
-				<c:if test="${empty list_mardi}">
+				<c:if test="${empty list_mardi[0]}">
 					<p>Pas de trajets disponibles ce jour la !</p>
 				</c:if>
-				<c:if test="${!empty list_mardi }">
-
-					<c:forEach var="rideshare" items="${list_mardi}">
-						<li>${rideshare.first_name} ${rideshare.last_name}
-							(${rideshare.email} / ${rideshare.phone})</li>
-
+				<c:if test="${!empty list_mardi[0] }">
+					 <c:forEach var="i" begin="0" end="${nb_trajets}" step="1">
+					 <p>Pour le trajet <c:out value="${trajets[i]}"/> :</p>
+						<c:forEach var="ride" items="${list_mardi[i]}">
+								<li>${ride.first_name} ${ride.last_name}
+									${ride.email} (${ride.email} / ${ride.phone})</li>
+						</c:forEach>
 					</c:forEach>
 				</c:if>
 			</ul>
-			<h3>Trajets possibles le mercredi avec :</h3>
+			<h3>Trajets possibles le mercredi :</h3>
 			<ul>
-
-				<c:if test="${empty list_mercredi}">
+				<c:if test="${empty list_mercredi[0]}">
 					<p>Pas de trajets disponibles ce jour la !</p>
 				</c:if>
-
-				<c:if test="${!empty list_mercredi }">
-
-					<c:forEach var="rideshare" items="${list_mercredi}">
-						<li>${rideshare.first_name} ${rideshare.last_name}
-							(${rideshare.email} / ${rideshare.phone})</li>
-
+				<c:if test="${!empty list_mercredi[0] }">
+					 <c:forEach var="i" begin="0" end="${nb_trajets}" step="1">
+					 <p>Pour le trajet <c:out value="${trajets[i]}"/> :</p>
+						<c:forEach var="ride" items="${list_mercredi[i]}">
+								<li>${ride.first_name} ${ride.last_name}
+									${ride.email} (${ride.email} / ${ride.phone})</li>
+						</c:forEach>
 					</c:forEach>
 				</c:if>
 			</ul>
-			<h3>Trajets possibles le jeudi avec :</h3>
+			<h3>Trajets possibles le jeudi :</h3>
 			<ul>
-				<c:if test="${empty list_jeudi}">
+				<c:if test="${empty list_jeudi[0]}">
 					<p>Pas de trajets disponibles ce jour la !</p>
 				</c:if>
-				<c:if test="${!empty list_lundi }">
-
-					<c:forEach var="rideshare" items="${list_jeudi}">
-						<li>${rideshare.first_name} ${rideshare.last_name}
-							(${rideshare.email} / ${rideshare.phone})</li>
-
+				<c:if test="${!empty list_jeudi[0] }">
+					 <c:forEach var="i" begin="0" end="${nb_trajets}" step="1">
+					 <p>Pour le trajet <c:out value="${trajets[i]}"/> :</p>
+						<c:forEach var="ride" items="${list_jeudi[i]}">
+								<li>${ride.first_name} ${ride.last_name}
+									${ride.email} (${ride.email} / ${ride.phone})</li>
+						</c:forEach>
 					</c:forEach>
 				</c:if>
 			</ul>
-			<h3>Trajets possibles le vendredi avec :</h3>
+			<h3>Trajets possibles le vendredi :</h3>
 			<ul>
-				<c:if test="${empty list_vendredi}">
+				<c:if test="${empty list_vendredi[0]}">
 					<p>Pas de trajets disponibles ce jour la !</p>
 				</c:if>
-				<c:if test="${!empty list_lundi }">
-
-					<c:forEach var="rideshare" items="${list_vendredi}">
-						<li>${rideshare.first_name} ${rideshare.last_name}
-							(${rideshare.email} / ${rideshare.phone})</li>
+				<c:if test="${!empty list_vendredi[0] }">
+					 <c:forEach var="i" begin="0" end="${nb_trajets}" step="1">
+					 <p>Pour le trajet <c:out value="${trajets[i]}"/> :</p>
+						<c:forEach var="ride" items="${list_vendredi[i]}">
+								<li>${ride.first_name} ${ride.last_name}
+									${ride.email} (${ride.email} / ${ride.phone})</li>
+						</c:forEach>
 					</c:forEach>
 				</c:if>
 			</ul>
 			<h3>Trajets possibles le samedi :</h3>
 			<ul>
-
-				<c:if test="${empty list_samedi}">
+				<c:if test="${empty list_samedi[0]}">
 					<p>Pas de trajets disponibles ce jour la !</p>
 				</c:if>
-				<c:if test="${!empty list_lundi }">
-
-					<c:forEach var="rideshare" items="${list_samedi}">
-						<li>${rideshare.first_name} ${rideshare.last_name}
-							(${rideshare.email} / ${rideshare.phone})</li>
-
+				<c:if test="${!empty list_samedi[0] }">
+					 <c:forEach var="i" begin="0" end="${nb_trajets}" step="1">
+					 <p>Pour le trajet <c:out value="${trajets[i]}"/> :</p>
+						<c:forEach var="ride" items="${list_samedi[i]}">
+								<li>${ride.first_name} ${ride.last_name}
+									${ride.email} (${ride.email} / ${ride.phone})</li>
+						</c:forEach>
 					</c:forEach>
 				</c:if>
 			</ul>
-			<h3>Trajets possibles le dimanche avec :</h3>
+			<h3>Trajets possibles le dimanche :</h3>
 			<ul>
-
-				<c:if test="${empty list_dimanche}">
+				<c:if test="${empty list_dimanche[0]}">
 					<p>Pas de trajets disponibles ce jour la !</p>
 				</c:if>
-				<c:if test="${!empty list_dimanche }">
-
-					<c:forEach var="rideshare" items="${list_dimanche}">
-						<li>${rideshare.first_name} ${rideshare.last_name}
-							${rideshare.email}</li>
-
+				<c:if test="${!empty list_dimanche[0] }">
+					 <c:forEach var="i" begin="0" end="${nb_trajets}" step="1">
+					 <p>Pour le trajet <c:out value="${trajets[i]}"/> :</p>
+						<c:forEach var="ride" items="${list_dimanche[i]}">
+								<li>${ride.first_name} ${ride.last_name}
+									${ride.email} (${ride.email} / ${ride.phone})</li>
+						</c:forEach>
 					</c:forEach>
 				</c:if>
 			</ul>
-
-
-
+			
 		</div>
 	</div>
 
--- SopRide/src/main/java/com/sopride/web/servlet/SearchForRideshare.java
+++ SopRide/src/main/java/com/sopride/web/servlet/SearchForRideshare.java
@@ -43,15 +43,32 @@ public class SearchForRideshare extends HttpServlet {
 		List<UserBE> list = DAO.getAllUser();
 		
 		
-		
 		if (user.getRide_infos().isEmpty()){
 			WebUtils.forward(request, response, "rideshareNotPossible.jsp");
 
 		}else{
-			
-		//for(RideInfoBE info_user : user.getRide_infos()){
-		RideInfoBE info_user = user.getRide_infos().get(0);
+		ArrayList<UserBE> matches_list_lundi[] = new ArrayList[user.getRide_infos().size()];
+		ArrayList<UserBE> matches_list_mardi[] = new ArrayList[user.getRide_infos().size()];
+		ArrayList<UserBE> matches_list_mercredi[] = new ArrayList[user.getRide_infos().size()];
+		ArrayList<UserBE> matches_list_jeudi[] = new ArrayList[user.getRide_infos().size()];
+		ArrayList<UserBE> matches_list_vendredi[] = new ArrayList[user.getRide_infos().size()];
+		ArrayList<UserBE> matches_list_samedi[] = new ArrayList[user.getRide_infos().size()];
+		ArrayList<UserBE> matches_list_dimanche[] = new ArrayList[user.getRide_infos().size()];
+		for(int j=0; j<user.getRide_infos().size(); j++){
+			matches_list_lundi[j] = new ArrayList<UserBE>();
+			matches_list_mardi[j] = new ArrayList<UserBE>();
+			matches_list_mercredi[j] = new ArrayList<UserBE>();
+			matches_list_jeudi[j] = new ArrayList<UserBE>();
+			matches_list_vendredi[j] = new ArrayList<UserBE>();
+			matches_list_samedi[j] = new ArrayList<UserBE>();
+			matches_list_dimanche[j] = new ArrayList<UserBE>();
+		}
 		
+		int i = 0;
+		String trajets[] = new String[user.getRide_infos().size()];
+		for(RideInfoBE info_user : user.getRide_infos()){
+		//RideInfoBE info_user = user.getRide_infos().get(0);
+		trajets[i] = "de " + info_user.getHome().toString() + " A " + info_user.getCar_pooling_workplace().toString() + " avec " ;
 		int heure_depart_matin = info_user.getMorning_hour().getHours();
 		int heure_depart_soir = info_user.getNight_hour().getHours();
 		int minute_depart = info_user.getMorning_hour().getMinutes();
@@ -60,51 +77,52 @@ public class SearchForRideshare extends HttpServlet {
 		int postcode = info_user.getHome().getPostCode();
 		WorkplaceBE workplace = info_user.getCar_pooling_workplace();
 		
-			List<UserBE> matches_list_lundi = new ArrayList<UserBE>();
+			/*List<UserBE> matches_list_lundi = new ArrayList<UserBE>();
 			List<UserBE> matches_list_mardi = new ArrayList<UserBE>();
 			List<UserBE> matches_list_mercredi = new ArrayList<UserBE>();
 			List<UserBE> matches_list_jeudi = new ArrayList<UserBE>();
 			List<UserBE> matches_list_vendredi = new ArrayList<UserBE>();
 			List<UserBE> matches_list_samedi = new ArrayList<UserBE>();
-			List<UserBE> matches_list_dimanche = new ArrayList<UserBE>();
+			List<UserBE> matches_list_dimanche = new ArrayList<UserBE>();*/
 			
 			for(UserBE user_aux : list){
 				if(user_aux != user && (user_aux.getRide_infos().size() > 0)){
-				RideInfoBE info = user_aux.getRide_infos().get(0);
+				for(RideInfoBE info : user_aux.getRide_infos()){
+				//RideInfoBE info = user_aux.getRide_infos().get(0);
 				if(heure_depart_matin == info.getMorning_hour().getHours() || (heure_depart_matin-1==info.getMorning_hour().getHours() && minute_depart <= info.getMorning_hour().getMinutes()) || (heure_depart_matin+1==info.getMorning_hour().getHours() && minute_depart >=info.getMorning_hour().getMinutes())){
 					if(heure_depart_soir == info.getNight_hour().getHours() || (heure_depart_soir-1==info.getNight_hour().getHours() && minute_depart_soir<=info.getNight_hour().getMinutes()) || (heure_depart_soir+1==info.getNight_hour().getHours() && minute_depart_soir>=info.getNight_hour().getMinutes())){
 						if (info_user.getDays().isLundi()){
 							if(info.getDays().isLundi()){
 								if((info.getHome().getPostCode() == postcode) && (info.getCar_pooling_workplace().equals(workplace))){
-									matches_list_lundi.add(user_aux);
+									matches_list_lundi[i].add(user_aux);
 								}						
 							}
 						}
 						if (info_user.getDays().isMardi()){
 							if(info.getDays().isMardi()){
 								if((info.getHome().getPostCode() == postcode) && (info.getCar_pooling_workplace().equals(workplace))){
-									matches_list_mardi.add(user_aux);
+									matches_list_mardi[i].add(user_aux);
 								}						
 							}
 						}
 						if (info_user.getDays().isMercredi()){
 							if(info.getDays().isMercredi()){
 								if((info.getHome().getPostCode() == postcode) && (info.getCar_pooling_workplace().equals(workplace))){
-									matches_list_mercredi.add(user_aux);						
+									matches_list_mercredi[i].add(user_aux);						
 								}						
 							}
 						}
 						if (info_user.getDays().isJeudi()){
 							if(info.getDays().isJeudi()){
 								if((info.getHome().getPostCode() == postcode) && (info.getCar_pooling_workplace().equals(workplace))){
-									matches_list_jeudi.add(user_aux);
+									matches_list_jeudi[i].add(user_aux);
 								}						
 							}
 						}
 						if (info_user.getDays().isVendredi()){
 							if(info.getDays().isVendredi()){
 								if((info.getHome().getPostCode() == postcode) && (info.getCar_pooling_workplace().equals(workplace))){
-										matches_list_vendredi.add(user_aux);
+									matches_list_vendredi[i].add(user_aux);
 								}						
 							}
 						}
@@ -112,25 +130,30 @@ public class SearchForRideshare extends HttpServlet {
 						if (info_user.getDays().isSamedi()){
 							if(info.getDays().isSamedi()){
 								if((info.getHome().getPostCode() == postcode) && (info.getCar_pooling_workplace().equals(workplace))){
-									matches_list_samedi.add(user_aux);
+									matches_list_samedi[i].add(user_aux);
 								}						
 							}
 						}
 						if (info_user.getDays().isDimanche()){
 							if(info.getDays().isDimanche()){
 								if((info.getHome().getPostCode() == postcode) && (info.getCar_pooling_workplace().equals(workplace))){
-										matches_list_dimanche.add(user_aux);
+									matches_list_dimanche[i].add(user_aux);
 								}						
 							}
 						}
 					}
-
-				
+					
+				 
 				}
 				}
-				
 				}
-						
+				}
+			i++;
+		}	
+			
+			int max =  user.getRide_infos().size() - 1;
+			request.setAttribute("trajets", trajets);
+			request.setAttribute("nb_trajets",max);
 			request.setAttribute("list_lundi", matches_list_lundi);
 			request.setAttribute("list_mardi", matches_list_mardi);
 			request.setAttribute("list_mercredi", matches_list_mercredi);
@@ -139,7 +162,8 @@ public class SearchForRideshare extends HttpServlet {
 			request.setAttribute("list_samedi", matches_list_samedi);
 			request.setAttribute("list_dimanche", matches_list_dimanche);
 			WebUtils.forward(request, response, "ridesharePossible.jsp");
-		//}
+			
+		
 		}
 				
 	}
